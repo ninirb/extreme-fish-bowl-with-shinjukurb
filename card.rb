51552583ed60e14484a839d7936ae7d472af3efd
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-12-26 20:47:41 +0900

    threetreeslight feat hkdnet

--- card.rb
+++ card.rb
@@ -1,6 +1,6 @@
 class Card
   include Comparable
-  attr_reader :number
+  attr_reader :number, :power
   attr_reader :mark
 
   def initialize(number, mark)
@@ -8,11 +8,16 @@ class Card
     @number = number.to_i
     # 歴史的経緯によりmark
     @mark = mark
-    @pwer
+    @power = case @number
+    when 1,2
+      @number + 13
+    else
+      @number
+    end
   end
 
   def <=>(other)
-    other.number - @number
+    other.power - @power
   end
 end
 
@@ -20,6 +25,15 @@ class Deck
 end
 
 raise unless (Card.new('2', 'heart') > Card.new('3', 'diamond'))
+raise unless (Card.new('4', 'heart') > Card.new('3', 'diamond'))
 
 raise unless (Card.new('1', 'heart').mark == Card.new('3', 'heart').mark)
 raise if (Card.new('1', 'diamond').mark == Card.new('3', 'heart').mark)
+
+
+
+
+
+
+
+
