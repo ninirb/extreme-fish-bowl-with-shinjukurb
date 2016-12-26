2ea3308816bac210865a0982a3d71dcd405a4090
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-12-26 20:27:46 +0900

    mtsmfm feat 5t111111

--- card.rb
+++ card.rb
@@ -1,2 +1,17 @@
-# TODO: MUST mark
+class Card
+  include Comparable
+  attr_reader :number
+
+  def initialize(number, mark)
+    # 歴史的経緯により文字列がくる
+    @number = number.to_i
+    # 歴史的経緯によりmark
+    @mark = mark
+  end
+
+  def <=>(other)
+    other.number - @number
+  end
+end
+
 raise unless (Card.new('2') > Card.new('3'))
