5d569227cf9f250f7968f4f4e98e6a77acfffdd8
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-12-26 21:38:15 +0900

    matsubokkuri feat. 5t111111

--- card.rb
+++ card.rb
@@ -21,8 +21,7 @@ class Deck
 
   def initialize
     @cards = SUITS.map do |suit|
-      (1..13).map { |num| Card.new(num, suit) }
-    end.flatten
+      (1..13).map { |num| Card.new(num, suit) } end.flatten
   end
 
   def shuffle
@@ -43,15 +42,4 @@ end
 
 player1 = Player.new
 player2 = Player.new
-
-raise unless (Card.new(2, 'heart') > Card.new(3, 'diamond'))
-raise unless (Card.new(4, 'heart') > Card.new(3, 'diamond'))
-
-raise unless (Card.new(1, 'heart').suit == Card.new(3, 'heart').suit)
-raise if (Card.new(1, 'diamond').suit == Card.new(3, 'heart').suit)
-
-raise unless (Deck.new.cards.length == 52)
-deck = Deck.new
-deck.pick!
-raise unless deck.cards.length == 51
 
