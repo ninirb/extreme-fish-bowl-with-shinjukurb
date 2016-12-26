751dae62c038e88856619fc138d5b1d43d5104ba
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-12-26 21:16:56 +0900

    matsubokkuri feat. treby

--- card.rb
+++ card.rb
@@ -6,7 +6,7 @@ class Card
   def initialize(number, suit)
     @number = number
     @suit = suit
-    @power = [2, 1, *(13..3)]
+    @power = ([2, 1] + (3..13).to_a.reverse).reverse
   end
 
   def <=>(other)
@@ -14,8 +14,16 @@ class Card
   end
 end
 
+class Deck
+  attr_reader :cards
+  def initialize(card)
+    @cards
+  end
+end
+
 raise unless (Card.new(2, 'heart') > Card.new(3, 'diamond'))
 raise unless (Card.new(4, 'heart') > Card.new(3, 'diamond'))
 
-raise unless (Card.new(1, 'heart').mark == Card.new(3, 'heart').mark)
-raise if (Card.new(1, 'diamond').mark == Card.new(3, 'heart').mark)
+raise unless (Card.new(1, 'heart').suit == Card.new(3, 'heart').suit)
+raise if (Card.new(1, 'diamond').suit == Card.new(3, 'heart').suit)
+
